296ccb6aca2fc1fc3af1c9f66fadc1c4f362bd85
j-hil <92876062+j-hil@users.noreply.github.com>
2022-09-28 00:54:24 +0100

    Move backend build to `setuptools`
    I'm more familiar with `setuptools` & it supports dynamic parameters
    in `pyproject.toml`.

--- src/jchess/__init__.py
+++ src/jchess/__init__.py
@@ -1,1 +1,10 @@
 """Init is required by some tooling."""
+
+from datetime import date
+
+def get_version():
+    today = date.today()
+    result = f"{today.year}.{today.month}.0"
+    print(result)
+
+__version__ = get_version()
