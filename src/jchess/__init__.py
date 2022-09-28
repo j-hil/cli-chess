ad1d2493ca0c4a6ff833c73fbce5b3dfcd45c04f
j-hil <92876062+j-hil@users.noreply.github.com>
2022-09-28 11:29:35 +0100

    Add versioning.
    Use convention `year.month.commits`.

--- src/jchess/__init__.py
+++ src/jchess/__init__.py
@@ -1,10 +1,30 @@
-"""Init is required by some tooling."""
+"""Provide versioning.
+
+The chosen convention is A.B.C where A, B, C are the year, month and number of commits
+respectively. This has the following desired properties:
+* generates a strictly increasing sequence of version numbers
+* completely automatic version number generation
+* naturally approximates the major.minor.patch convention
+* pep 440 compliant
+* provides useful information at a glance
+
+The version is also tagged as dirty if there are uncommitted changes.
+"""
 
 from datetime import date
+from subprocess import run
 
-def get_version():
+
+def _get_stdout(cmd: str) -> str:
+    return run(cmd.split(), capture_output=True).stdout.decode()
+
+
+def _get_version():
     today = date.today()
-    result = f"{today.year}.{today.month}.0"
-    print(result)
+    # this probably only works with a linear commit history
+    commits = _get_stdout("git rev-list --all --count").strip()
+    dirty = _get_stdout("git status --porcelain") and "+dirty"
+    return f"{today.year}.{today.month}.{commits}{dirty}"
 
-__version__ = get_version()
+
+__version__ = _get_version()
